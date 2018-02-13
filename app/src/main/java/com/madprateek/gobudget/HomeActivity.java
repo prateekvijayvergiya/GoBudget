4f9e9b440a30efad3e08712ceba9e75436fab850
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2018-02-14 00:48:57 +0530

    drawer added

--- app/src/main/java/com/madprateek/gobudget/HomeActivity.java
+++ app/src/main/java/com/madprateek/gobudget/HomeActivity.java
@@ -1,13 +1,42 @@
 package com.madprateek.gobudget;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 public class HomeActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToogle;
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.homeAppBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("GoBudget");
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToogle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToogle.onOptionsItemSelected(item)){
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
